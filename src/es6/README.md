21ffbf4fabefc01dfb8358aa19b9b683532c86bc
Jorgelisapa <jorgelisapa@gmail.com>
2020-02-18 13:00:39 -0500

    Class: LET, CONST, multiline, Spread operator and destructuring - v3

---
+++ src/es6/README.md
@@ -0,0 +1,28 @@
+# Title 1
+## Title 2
+### Title 3
+#### Title 4
+##### Title 5
+###### Title 6
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](detinationLink)
+
+-
+*
+[Nombre de enlace]
+(enlace)
+
+
+```
+bloque de código
+```
+
+| Tabla | Tabla | Tabla |
+|-------|-------|-------|
+| dato | otro dato | más datos |
+| aquí mas | Así puedo continuar | uff que tablota tan bonita cool
+
+
+
+
+
--- src/es6/index.js
+++ src/es6/index.js
@@ -33,3 +33,68 @@ console.log(epicPhrase);
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
 
+
+
+
+// Class: LET, CONST, multiline, Spread operator and destructuring - v3
+
+//Multiline
+//Before...
+var lorem = 'Esta es una frase super buena mostando la forma anterior a es6x de como separar lineas de texto \n' 
++ 'para este proyecto.'
+console.log(lorem)
+
+// Now with es6x...
+let lorem2 = `Esta es una nueva version para hacer este tipo de cosas como separar lineas de texto. 
+Que te parece?`;
+console.log(lorem2)
+
+
+//DESTRUCTURING
+let person = {
+    'name': 'Jorge',
+    'age': 32,
+    'country': 'COL'
+}
+// Before...
+console.log(person.name, person.age, person.country);
+//Destructure elements es6x...
+let { name, age, country } = person;
+console.log(name, age);
+
+
+
+// SPREAD OPERATOR (Operador de propagación)
+let team1 = ['Jorge', 'Diego', 'Pali'];
+let team2 = ['Banzell', 'Megan', 'Ballack'];
+
+let education = ['Omar', ...team1, ...team2];
+
+console.log(education);
+
+
+
+// VAR or LET
+//Before...
+{
+    var globalVar = 'globar var';
+}
+console.log(globalVar);
+
+
+//Now with es6x - The scope or context gives up to the code block itself
+{
+    let globalLet = 'global let';
+    console.log(globalLet);
+}
+
+// Before...
+var a = 'b';
+a = 'a';
+console.log(a);
+
+// Now with es6x
+// In this way the variable does not change since it is constant
+const a = 'b';
+a = 'a';
+console.log(a);
